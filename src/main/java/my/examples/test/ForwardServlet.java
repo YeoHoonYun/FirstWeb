24d42dbd2e32c627f00a4a37d5fd8abf75f0b3b2
yun <cjswo9207@gmail.com>
2019-01-10 12:23:12 +0900

    docs : cookie, session

--- src/main/java/my/examples/test/ForwardServlet.java
+++ src/main/java/my/examples/test/ForwardServlet.java
@@ -3,9 +3,7 @@ package my.examples.test;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 /**
